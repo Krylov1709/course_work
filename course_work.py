5876a75441b9b8a6d4de948d4b513aef6c086966
Крылов Алексей <krylov1709@yandex.ru>
2022-07-07 22:41:20 +0300

    Испарвление замечаний 1

--- course_work.py
+++ course_work.py
@@ -17,24 +17,41 @@ class VK:
         url = 'https://api.vk.com/method/users.get'
         params = {'user_ids': self.id}
         response = requests.get(url, params={**self.params, **params})
-        return response.json()
+        if response.status_code != 200:
+            print ('Ошибка получения ответа от сервера')
+        try:
+            response = response.json()
+            id = response['response'][0]['id']
+            return id
+        except KeyError:
+            print ('Ошибка ввода ID. Будет использован ваш собственный ID') 
+        except IndexError:
+            print ('Ошибка ввода ID. Будет использован ваш собственный ID')
    
     def photos_info(self):
         '''Вызываем список фотографий в профиле и сохраняем в список максимальный размер, количество лайков и дату фотографии'''
         url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': self.id, 
+        params = {'owner_id': self.users_info(), 
                   'album_id': 'profile',
                   'extended': 1}
-        response = requests.get(url, params={**self.params, **params}).json()
-        photo_dict = {}
-        photos_list = []
-        for photo in tqdm(response['response']['items']):
-            photo_dict =  {'likes': photo['likes']['count'], 
-                           'url': photo['sizes'][-1]['url'], 
-                           'date': datetime.datetime.fromtimestamp(photo['date']),
+        response = requests.get(url, params={**self.params, **params})
+        if response.status_code != 200:
+            print ('Ошибка')
+        try:    
+            response = response.json()
+            photo_dict = {}
+            photos_list = []
+            for photo in tqdm(response['response']['items']):
+                photo_dict =  {'likes': photo['likes']['count'], 
+                                'url': photo['sizes'][-1]['url'], 
+                            'date': datetime.datetime.fromtimestamp(photo['date']),
                            'size': photo['sizes'][-1]['type']}
-            photos_list.append(photo_dict)
-        return photos_list
+                photos_list.append(photo_dict)                
+            return photos_list
+        except KeyError:
+            print ('Неверно полученные данные с сервера') 
+        except IndexError:
+            print ('Неверно полученные данные с сервера')
    
 class Yandex:
     
@@ -46,7 +63,14 @@ class Yandex:
     def create_folder(self, path):
         '''Создаем папку на яндекс диске'''
         url = f'https://cloud-api.yandex.net/v1/disk/resources?path={path}'
-        return requests.put(url, headers=self.headers)
+        response = requests.put(url, headers=self.headers)
+        if response.status_code == 409:
+            print('Такой альбом уже существует.')
+        elif response.status_code != 201:
+            print('Ошибка обработки сервера')
+        else:
+            print(f'Создан альбом "{path}"')    
+            return response
          
     def load_file(self, path):
         '''Записываем файл в указанную папку на яндекс диске и создаем файл с информацией о сохраненных файлах'''
@@ -54,36 +78,44 @@ class Yandex:
         size = []
         file_name = []
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        for photo in tqdm(vk.photos_info()):
-            url_photo = photo['url']
-            size.append(photo['size'])
-            if f'{photo["likes"]}.jpg' in file_name:
-                file_name_photo = photo['date'].strftime(f'{photo["likes"]}_%Y_%m_%d_%H_%M_%S.jpg')
-                file_name.append(file_name_photo)
-            else:
-                file_name_photo = f'{photo["likes"]}.jpg'
-                file_name.append(file_name_photo)          
-            params = {'path': f'{path}/{file_name_photo}',
-                      'url': url_photo}
-            response = requests.post(url, params=params, headers=self.headers)
-        copy_result = [dict(file_name = file_name, size = size) for file_name, size in zip(file_name, size)]
-        return copy_result
+        try:
+            for photo in tqdm(vk.photos_info()):
+                url_photo = photo['url']
+                size.append(photo['size'])
+                if f'{photo["likes"]}.jpg' in file_name:
+                    file_name_photo = photo['date'].strftime(f'{photo["likes"]}_%Y_%m_%d_%H_%M_%S.jpg')
+                    file_name.append(file_name_photo)
+                else:
+                    file_name_photo = f'{photo["likes"]}.jpg'
+                    file_name.append(file_name_photo)          
+                params = {'path': f'{path}/{file_name_photo}',
+                            'url': url_photo}
+                response = requests.post(url, params=params, headers=self.headers)
+            print('Копирование файлов завершено')
+            copy_result = [dict(file_name = file_name, size = size) for file_name, size in zip(file_name, size)]
+            return copy_result
+        except TypeError:
+            print('Не веррные данные для загрузки')
     
 def copy_result(copy_result):
-    file = json.dumps(list(copy_result))
-    file = json.loads(str(file))
-    with open("copy_result.json", "w", encoding = 'utf-8') as write_file:
-        json.dump(file, write_file, indent = 0)
-        print('Запись выполнена')
+    try:
+        file = json.dumps(list(copy_result))
+        file = json.loads(str(file))
+        with open("copy_result.json", "w", encoding = 'utf-8') as write_file:
+            json.dump(file, write_file, indent = 0)
+            print('Запись выполнена')
+    except TypeError:
+        print('Неверный формат данных для записи в файл')        
 
 ya_access_token = ''
 vk_access_token = ''
 user_id = input('Введите ID пользователя: ')
 vk = VK(vk_access_token, user_id)
 ya = Yandex(ya_access_token)
-name_album_yandex = input('Введите названия альбома на Яндекс Диске: ')
+name_album_yandex = input('Введите название альбома для сохранения: ')
 # pprint(vk.users_info())
 # pprint(vk.photos_info())
-# ya.load_file("Мой альбом")
+# ya.create_folder(name_album_yandex)
+# ya.load_file(name_album_yandex)
 copy_result(ya.load_file(name_album_yandex))
 
